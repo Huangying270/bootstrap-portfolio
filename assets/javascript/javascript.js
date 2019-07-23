4ad0a5d0f899f80ac226e972f53144f1835f9f03
Ying Huang <huangying270@gmail.com>
2019-07-23 01:55:34 -0400

    updated portfolio and images

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-    $(".crystalCollector").on("click", function(){
-        src="https://huangying270.github.io/unit-4-game/";
+    $(".projects").on("click", function(){
+        
     })
 
 })
